788ad7091135010849a8c353a513a10552d8dbc1
21-2918-830 Christian Paul Figuracion <christianpaul.figuracion@cit.edu>
2024-11-30 15:05:51 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+      <div className="navbar bg-base-100">
+        <a className="btn btn-ghost text-xl">JCC RainShelter</a>
+      </div>
+
+      <div className="p-5">
+       <button className="btn btn-outline btn-success">Success</button>
+      </div>
+    </>
+  )
+}
+
+export default App
